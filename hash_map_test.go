6fb260de947d9d8aa2e1f6a7fd57093d87376877
chrisandrews7 <chrisandrews7@users.noreply.github.com>
2020-04-21 09:31:44 +0100

    breakup tests for hashmap

--- hash_map_test.go
+++ hash_map_test.go
@@ -9,10 +9,8 @@ func TestHashMap(t *testing.T) {
 	hashmap := NewHashMap(primeNumber)
 
 	hashmap.Set("fish", "chips")
-	hashmap.Set("mushy", "pea")
-	hashmap.Set("mushy", "peas") // Test overriding
+	hashmap.Set("mushy", "peas")
 	hashmap.Set("salt", "vinegar")
-	hashmap.Set("salt2", "pepper") // Same key hash - Testing collisions
 
 	if hashmap.Get("fish") != "chips" {
 		t.Error("Expected the value chips")
@@ -20,6 +18,30 @@ func TestHashMap(t *testing.T) {
 	if hashmap.Get("mushy") != "peas" {
 		t.Error("Expected the value peas")
 	}
+	if hashmap.Get("salt") != "vinegar" {
+		t.Error("Expected the value vinegar")
+	}
+}
+
+func TestHashMapOverride(t *testing.T) {
+	primeNumber := 17
+	hashmap := NewHashMap(primeNumber)
+
+	hashmap.Set("mushy", "pea")
+	hashmap.Set("mushy", "peas")
+
+	if hashmap.Get("mushy") != "peas" {
+		t.Error("Expected the value peas")
+	}
+}
+
+func TestHashMapCollisions(t *testing.T) {
+	primeNumber := 17
+	hashmap := NewHashMap(primeNumber)
+
+	hashmap.Set("salt", "vinegar")
+	hashmap.Set("salt2", "pepper")
+
 	if hashmap.Get("salt") != "vinegar" {
 		t.Error("Expected the value vinegar")
 	}
